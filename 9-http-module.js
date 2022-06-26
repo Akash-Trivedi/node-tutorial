830413b02f2c2321e2fe8d348d6449311cae0a66
Akash-Trivedi <sunnytrivedi321@gmail.com>
2022-06-26 22:52:46 +0530

    http done

--- 9-http-module.js
+++ 9-http-module.js
@@ -3,3 +3,19 @@
  * contains some core features of nodejs
  */
 
+const http = require('http')
+
+const server = http.createServer(
+    (request, response) => {
+        // console.log(request); huge object
+        if (request.url === '/') {
+            response.end('welcome to the homepage')
+            // end plus give the message
+        }
+        // below is a basic response for unknown path
+        response.write('"your request was not good": server')
+        response.end()
+    }
+)
+// we can also pass the html string like servlets
+server.listen(8083)
